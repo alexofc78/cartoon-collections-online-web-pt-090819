fee1896c7de7ed3b8b6a7a969b7b3ba14022b648
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-23 04:12:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,13 +10,20 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls(calls_long)
-  if calls_long.length > 4.to eq(true)
-  end
+def long_planeteer_calls(array)
+  i = 0 
+  if  array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end 
 end
 
 def find_the_cheese(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.include?()
-  
+    array.find do |type|
+    cheese_types.include?(type)
+  end
 end
+
