9fd5467a56ac67d3b8a7bb8279b4a4179406b547
Alexandra Oliveros <alexofc@gmail.com>
2020-05-14 18:20:06 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,26 +4,26 @@ def roll_call_dwarves(names)
   end
 end
 
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect do |calls|
-    calls.capitalize + "!"
-  end
-end
+ def summon_captain_planet(planeteer_calls)
+   planeteer_calls.collect do |calls|
+     calls.capitalize + "!"
+   end
+ end
 
-def long_planeteer_calls(array)
-  i = 0 
-  if  array.any? {|i| i.length > 4}
-    return true
-  else 
-    return false
-  i = i + 1
-  end 
-end
+ def long_planeteer_calls(array)
+   i = 0 
+   if  array.any? {|i| i.length > 4}
+     return true
+   else 
+     return false
+   i = i + 1
+   end 
+ end
 
-def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-    array.find do |type|
-    cheese_types.include?(type)
-  end
-end
+ def find_the_cheese(array)
+   cheese_types = ["cheddar", "gouda", "camembert"]
+     array.find do |type|
+     cheese_types.include?(type)
+   end
+ end
 
